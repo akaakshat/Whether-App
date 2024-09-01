6c2eeea7bec61190e06c0a8fddb4e0d95c262479
Akshat bhatnagar <168410064+akaakshat@users.noreply.github.com>
2024-09-01 19:32:25 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5ec40deae75271e5b0ef6868f47342c9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
